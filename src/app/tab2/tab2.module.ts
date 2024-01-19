a17785640af53b13b65e397dc6ffd5ca931730ef
ssssssss <63939907+Santi1206760@users.noreply.github.com>
2024-01-19 18:35:21 -0500

    Finally Commit

---
+++ src/app/tab2/tab2.module.ts
@@ -0,0 +1,22 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab2Page } from './tab2.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { UserCardComponent } from '../user-card/user-card.component';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ExploreContainerComponentModule,
+    Tab2PageRoutingModule
+  ],
+  declarations: [Tab2Page,NavBarComponent,UserCardComponent]
+})
+export class Tab2PageModule {}
---
+++ src/app/user-card/user-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['user-card.component.scss']
+
+})
+export class UserCardComponent {
+
+  @Input() image = '';
+  @Input() newscaster = '';
+  @Input() title= '';
+  @Input() time= '';
+
+}
